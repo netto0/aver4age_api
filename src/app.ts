ebcb2cf78d24d62d0169099cee23750ec475a2c8
Netto <orlandiomnetto@gmail.com>
2024-06-12 07:32:38 -0300

    Inserção e configuração CORS lib

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,14 @@ import config from 'config'
 import router from './router'
 import db from '../config/db'
 
+var cors = require('cors')
 const app = express()
 app.use(express.json())
 
 const port = config.get<number>("port")
 
+app.use(cors())
 app.use('/average_api/', router)
-
 app.listen(port, async () => {
     await db()
     console.log(`Aplicação rodando na porta ${port}`)
