b7e60cc63db086f0e47e6d1d6c107ce02dc2a930
Netto <orlandiomnetto@gmail.com>
2024-09-27 06:57:07 -0300

    Revert "- Teste api vercel"
    This reverts commit bf306475585a01b5e0b35f59004ebb2b15dc30be.

--- config/default.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
-exports.default = {
-    port: 3000,
-    dbUri: `mongodb+srv://${dbUser}:${dbPassword}@cursots.ynnuyze.mongodb.net/?retryWrites=true&w=majority&appName=CursoTS`,
-    env: 'development'
-};
