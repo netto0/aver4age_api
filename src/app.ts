a96d620262f69c5156790e3b117c88199395b96a
Netto <orlandiomnetto@gmail.com>
2024-09-28 07:25:00 -0300

    -a

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
-require('dotenv').config()
-import express from 'express'
-import config from 'config'
-import router from './router'
-import db from '../config/db'
+// require('dotenv').config()
+// import express from 'express'
+// import config from 'config'
+// import router from './router'
+// import db from '../config/db'
 
-var cors = require('cors')
-const app = express()
-app.use(express.json())
+// var cors = require('cors')
+// const app = express()
+// app.use(express.json())
 
-const port = config.get<number>("port")
+// const port = config.get<number>("port")
 
-app.use(cors())
-app.use('/average_api/', router)
-app.listen(port, async () => {
-    await db()
-    console.log(`Aplicação rodando na porta ${port}`)
-})
+// app.use(cors())
+// app.use('/average_api/', router)
+// app.listen(port, async () => {
+//     await db()
+//     console.log(`Aplicação rodando na porta ${port}`)
+// })
