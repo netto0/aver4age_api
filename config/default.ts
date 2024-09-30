3fd8bd39969671b02887777e0f57bddc90698fd4
Netto <orlandiomnetto@gmail.com>
2024-09-30 07:24:59 -0300

    - Rota teste funcionando na vercel

--- config/default.ts
+++ config/default.ts
@@ -2,7 +2,12 @@ const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 
 export default {
-    port: 3000,
+    // port: 3000,
     dbUri: `mongodb+srv://${dbUser}:${dbPassword}@cursots.ynnuyze.mongodb.net/?retryWrites=true&w=majority&appName=CursoTS`,
-    env: 'development'
+    env: 'production'
 }
+// export default {
+//     port: 3000,
+//     dbUri: `mongodb+srv://${dbUser}:${dbPassword}@cursots.ynnuyze.mongodb.net/?retryWrites=true&w=majority&appName=CursoTS`,
+//     env: 'development'
+// }
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
-// require('dotenv').config()
-// import express from 'express'
+require('dotenv').config()
+import express from 'express'
 // import config from 'config'
-// import router from './router'
-// import db from '../config/db'
+import router from './router'
+import db from '../config/db'
 
-// var cors = require('cors')
-// const app = express()
-// app.use(express.json())
+var cors = require('cors')
+const app = express()
+app.use(express.json())
 
 // const port = config.get<number>("port")
 
-// app.use(cors())
-// app.use('/average_api/', router)
+app.use(cors())
+app.use('/average_api/', router)
+app.listen(3000, async () => {
+    await db()
+    console.log(`Aplicação rodando na porta ${3000}`)
+})
 // app.listen(port, async () => {
 //     await db()
 //     console.log(`Aplicação rodando na porta ${port}`)
