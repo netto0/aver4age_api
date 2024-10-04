6fdb2c200518ab9e495ece6525982b8d4ec8aa2c
Netto <orlandiomnetto@gmail.com>
2024-10-04 07:01:09 -0300

    Outro teste

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 require("dotenv").config();
 import express from "express";
-// import config from 'config'
 import router from "./router";
 import db from "../config/db";
 
 var cors = require("cors");
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:5173", "http://127.0.0.1:5173"] }));
-// app.use(cors());
+app.use(cors());
 app.use("/average_api/", router);
-app.listen(3000, async () => {
+
+app.listen(4000, async () => {
   await db();
-  console.log(`Aplicação rodando na porta ${3000}`);
+  console.log(`Aplicação rodando na porta 3000`);
 });
+
+export default app
